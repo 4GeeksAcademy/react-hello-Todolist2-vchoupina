248d3b7854f60e26b567ea6ca948968d24774c1d
vchoupina <168114326+vchoupina@users.noreply.github.com>
2024-07-17 17:46:21 +0000

    to-do

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import "../../styles/index.css";
 
 const Home = () => {
 	const [todos, setTodos] = useState([]);
 	const [newTodo, setNewTodo] = useState("");
 
-	// Função para adicionar uma nova tarefa
+	// Função para nova tarefa
 	const handleAddTodo = (e) => {
 		if (e.key === "Enter" && newTodo.trim() !== "") {
 			setTodos([...todos, newTodo]);
@@ -12,51 +13,44 @@ const Home = () => {
 		}
 	};
 
-	// Função para remover uma tarefa
+	// Função para eliminar tarefa
 	const handleRemoveTodo = (index) => {
-		const updatedTodos = todos.filter((todos, i) => i !== index);
+		const updatedTodos = todos.filter((todo, i) => i !== index);
 		setTodos(updatedTodos);
 	};
 
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Todos</h1>
-
+		<div className="container">
+			<h1>To Do</h1>
 			<div className="content">
-				{/* Input */}
 				<input
 					type="text"
-					value={newTodo} // Valor do campo de entrada vem do estado
-					onChange={(e) => setNewTodo(e.target.value)} // Atualiza o estado quando o usuário digita
-					onKeyDown={handleAddTodo} // Adiciona a tarefa quando o usuário pressiona Enter
-					placeholder="Your tasks" 
+					value={newTodo}
+					onChange={(e) => setNewTodo(e.target.value)}
+					onKeyDown={handleAddTodo}
+					placeholder="What needs to be done?"
 				/>
+				<ul>
+					{todos.length === 0 ? (
+						<li className="no-tasks">No tasks yet</li>
+					) : (
+						todos.map((todo, index) => (
+							<li key={index} className="todo-item">
+								{todo}
+								<span
+									className="remove-icon"
+									onClick={() => handleRemoveTodo(index)}
+								>
+									X
+								</span>
+							</li>
+						))
+					)}
+				</ul>
+				<div className="footer">
+					{todos.length > 0 && <span>{todos.length} item left</span>}
+				</div>
 			</div>
-
-
-			<ul>
-
-				{todos.length === 0 ? (
-					<li>Add tasksNo hay tareas, añadir tareas</li>
-				) : (
-
-					todos.map((todo, index) => (
-						<li key={index}
-							onMouseEnter={() => document.getElementById(`remove-${index}`).style.display = 'inline'}
-							onMouseLeave={() => document.getElementById(`remove-${index}`).style.display = 'none'}
-						>
-							{todo}
-							<span
-								id={`remove-${index}`}
-								style={{ display: 'none', cursor: 'pointer' }}
-								onClick={() => handleRemoveTodo(index)}
-							>
-								❌
-							</span>
-						</li>
-					))
-				)}
-			</ul>
 		</div>
 	);
 };
